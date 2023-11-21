d9b1c4f89253b37131cbe7ad84f0bb85ef38033a
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-11-21 19:35:42 +0100

    Added Jetstream and Livewire

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+	server: {
+	    hmr: {
+		host: '127.0.0.1'
+	    }
+	},
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+            ],
         }),
     ],
 });
