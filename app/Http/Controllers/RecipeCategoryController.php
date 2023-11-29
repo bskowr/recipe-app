a76628b47bef0994c3de586142cb39e3d5121d31
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-11-29 21:37:10 +0100

    Fixed RecipeCategoryController

--- app/Http/Controllers/RecipeCategoryController.php
+++ app/Http/Controllers/RecipeCategoryController.php
@@ -14,7 +14,7 @@ class RecipeCategoryController extends Controller
      */
     public function create(){
         return view(
-            'categories.form'
+            'recipe_categories.form'
         );
     }
 
