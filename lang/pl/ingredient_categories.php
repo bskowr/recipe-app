0aa86cdc4655585b1f10045aba3d887f23fe2ede
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-11-30 18:56:34 +0100

    Created translation

---
+++ lang/pl/ingredient_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'messages' => [
+        'successes' => [
+            'soft_delete' => [
+                'title' => 'Usunięto',
+                'description' => 'Usunięto kategorię składnika :name',
+            ],
+        ],
+    ],
+    'dialogs' => [
+        'soft_delete' => [
+            'title' => 'Potwierdzenie usunięcia',
+            'description' => 'Czy na pewno usunąć kategorię składnika :name?',
+        ],
+    ],
+    'labels' => [
+        'create_form_title' => 'Utwórz kategorię składnika',
+        'edit_form_title' => 'Edytuj kategorię składnika',
+    ],
+    'actions' => [
+        'create' => "Dodaj kategorię składnika",
+        'edit' => 'Edytuj kategorię składnika',
+        'soft_delete' => 'Usuń kategorię składnika',
+    ],
+    'attributes' => [
+        'name' => 'Nazwa kategorii',
+    ],
+];
