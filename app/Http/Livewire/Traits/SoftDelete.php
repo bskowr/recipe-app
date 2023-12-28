c61e175931f941a08e5fe21954b01bc00258ae2c
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-12-28 19:19:15 +0100

    Implemented SoftDelete trait

--- app/Http/Livewire/Traits/SoftDelete.php
+++ app/Http/Livewire/Traits/SoftDelete.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Livewire\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait SoftDelete
 {
+    protected function softDeleteNotificationTitle(){
+        return __('translation.messages.success.soft_delete.title');
+    }
 
+    protected function softDeleteNotificationDescription(Model $model){
+        return __('translation.messages.success.soft_delete.description', ['model' => $model]);
+    }
+
+    public function softDelete($id){
+        $model = $this->model->find($id);
+        $model->delete();
+        $this->notification()->success(
+            $title = $this->softDeleteNotificationTitle(),
+            $description = $this->softDeleteNotificationDescription($model)
+        );
+    }
 }
