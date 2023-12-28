f07094a535ac9f1b7efff78bbb68513c839ab1e0
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-12-28 19:14:41 +0100

    Refactoring start

---
+++ app/Http/Livewire/Actions/SoftDeleteAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire\Actions;
+use LaravelViews\Views\View;
+use LaravelViews\Actions\Action;
+use Illuminate\Database\Eloquent\Model;
+
+class SoftDeleteAction extends Action
+{
+    public $title = '';
+    public $icon = 'trash-2';
+
+    public function __construct(?string $title = null){
+        if ($title != null) {
+            $this->title = $title;
+        }
+        else {
+            $this-> title = __('translation.actions.soft_delete');
+        }
+    }
+
+    protected function dialogTitle() : String{
+        return __('translation.dialogs.soft_delete.title');
+    }
+
+    protected function dialogDescription(Model $model) : String{
+        return __('translation.dialogs.soft_delete.description', ['model' => $model]);
+    }
+
+    public function handle($model, View $view){
+        $view->dialog()->confirm([
+            'title' => $this->dialogTitle(),
+            'description' => $this->dialogDescription($model),
+            'icon' => 'question',
+            'iconColor' => 'text-red-500',
+            'accept' => [
+                'label' => __('translation.yes'),
+                'method' => 'softDelete',
+                'params' => $model->id,
+            ],
+            'reject' => [
+                'label' => __('translation.no'),
+            ]
+        ]);
+    }
+
+    public function renderIf($model, View $view){
+        return request()->user()->can('delete', $model);
+    }
+}
---
+++ lang/en/translation.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'yes' => 'Yes',
+    'no' => 'No',
+    'back' => 'Back',
+    'save' => 'Save',
+    'enter' => 'Enter value',
+    'actions' => [
+        'soft_delete' => 'Object deletion',
+    ],
+    'dialogs' => [
+        'soft_delete' => [
+            'title' => 'Object deletion',
+            'description' => 'Do you want to delete :model?'
+        ],
+    ],
+    'messages' => [
+        'successes' => [
+            'updated_title' => 'Updated',
+            'updated' => 'Updated :name',
+            'stored_title' => 'Saved',
+            'stored'   => 'Saved :name'
+        ],
+    ],
+    'attributes' => [
+        'created_at' => 'Created at',
+        'updated_at' => 'Updated at',
+        'deleted_at' => 'Deleted at',
+    ],
+    'navigation' => [
+        'dashboard' => 'Dashboard',
+        'users' => 'Users',
+        'recipe_categories' => 'Recipe categories',
+        'ingredient_categories' => 'Ingredent categories',
+    ],
+];
