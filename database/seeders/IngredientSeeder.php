f27417847243ba9e803aa5549df82daaed30b53e
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-12-30 16:59:41 +0100

    Added model and ingredient seeder

---
+++ database/seeders/IngredientSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ingredient;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class IngredientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Ingredient::factory()->count(100)->create();
+    }
+}
