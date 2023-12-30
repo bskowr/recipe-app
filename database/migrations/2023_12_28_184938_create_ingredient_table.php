068d67c33a9a443be3b0aea3405b4f56271647ba
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2023-12-30 16:40:49 +0100

    Implemented ingredients table

--- database/migrations/2023_12_28_184938_create_ingredient_table.php
+++ database/migrations/2023_12_28_184938_create_ingredient_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('ingredient', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('description', 512);
+            $table->unsignedBigInteger('ingredient_category_id');
+            $table->foreign('ingredient_category_id')->references('id')->on('ingredient_category')->onDelete('no action');
+            $table->decimal('price', 8, 2);
+            $table->decimal('owned_amount', 8, 2);
+            $table->string('unit', 8)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
