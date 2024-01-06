8bd2550a7b59536c411d46833be8ac520dce7681
Bartosz Skowroński <skowronski.bartoszz@gmail.com>
2024-01-06 18:24:08 +0100

    Created neccessary classes

---
+++ app/Http/Livewire/Recipe/RecipeForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Recipe;
+
+use Livewire\Component;
+
+class RecipeForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.recipe.recipe-form');
+    }
+}
---
+++ app/Http/Livewire/Recipe/RecipeGridView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Recipe;
+
+use Livewire\Component;
+
+class RecipeGridView extends Component
+{
+    public function render()
+    {
+        return view('livewire.recipe.recipe-grid-view');
+    }
+}
